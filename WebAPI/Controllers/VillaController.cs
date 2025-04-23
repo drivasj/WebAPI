eb0274736ad4423c9151f12ebeef6569cce4306f
[Daniel Rj] <[darivasjt@gmail.com]>
2025-04-23 16:01:10 -0400

    Villa ini

---
+++ WebAPI/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Vista a la Piscina" },
+                new Villa { Id = 2, Name = "Vista a la Playa" }
+            };
+        }
+    }
+}
