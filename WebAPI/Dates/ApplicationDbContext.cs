8a7f5f7c2dc741da80b1056ef037eeaf5733c7a0
[Daniel Rj] <[darivasjt@gmail.com]>
2025-05-16 15:48:27 -0400

    Connection , create table Villa

---
+++ WebAPI/Dates/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+
+namespace WebAPI.Dates
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- WebAPI/Models/Villa.cs
+++ WebAPI/Models/Villa.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebAPI.Models
 {
     public class Villa
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
 
