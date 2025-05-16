abd10d90fac5b1c508e040ec6a91080550898706
[Daniel Rj] <[darivasjt@gmail.com]>
2025-05-16 11:04:46 -0400

    add field in entity Villa

--- WebAPI/Models/Dto/VillaDto.cs
+++ WebAPI/Models/Dto/VillaDto.cs
@@ -10,8 +10,24 @@ namespace WebAPI.Models.Dto
         [MaxLength(30)]
         public string Name { get; set; }
 
-        public int Ocupantes { get; set; }
-        public int MetrosCuadrados { get; set; }
+        public string Detail { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public int Occupants { get; set; }
+
+        public double SquareMeters { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string RegisterUser { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public string LastUpdateUser { get; set; }
+
+        public DateTime LastUpdate { get; set; }
 
     }
 }
--- WebAPI/Models/Villa.cs
+++ WebAPI/Models/Villa.cs
@@ -1,8 +1,31 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
 {
     public class Villa
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string Detail { get; set; }
+
+        [Required]
+        public double Price {  get; set; }
+
+        public int Occupants { get; set; }
+
+        public double SquareMeters {  get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string RegisterUser { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public string LastUpdateUser {  get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
     }
 }
